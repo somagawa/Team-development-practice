febcea4ad2ab4eca38b7ab9884d027e0bcc40bfb
堀田優介 <somagawa645@gmail.com>
2020-03-31 16:57:09 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
