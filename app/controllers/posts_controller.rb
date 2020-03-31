53f1458c9beb26a26a27dc503d8df7629502ada3
堀田優介 <yuusuke@horita-no-MacBook-Air.local>
2020-03-31 19:07:44 +0900

    記事投稿機能、記事詳細表示機能、記事一覧表示機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < ApplicationController
-  def index
+  def new
+  	@post = Post.new
   end
 
-  def new
+  def create
+  	@post = Post.new(post_params)
+  	if @post.save
+  		redirect_to post_path(@post)
+  	end
+  end
+
+  def index
+  	@posts = Post.all
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :body)
   end
 end
