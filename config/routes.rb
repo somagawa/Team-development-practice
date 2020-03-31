9a36b301fb2e8ed4b10aa93d45f053934e260c68
堀田優介 <somagawa645@gmail.com>
2020-03-31 19:17:12 +0900

    ルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'users/show'
+  resources :users, only: [:new, :create, :show,]
+  resources :post, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
