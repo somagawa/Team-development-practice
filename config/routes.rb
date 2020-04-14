e5a51cafb8bfe4a6c0b5026f66c6033651786a53
堀田優介 <somagawa645@gmail.com>
2020-04-15 01:40:09 +0900

    create book model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :book, only [:index, :new, :create, :show, :edit, :update]
   resources :users, only: [:new, :create, :show,]
   resources :post, only: [:new, :create, :index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
